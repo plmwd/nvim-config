e455742af51ba385909631a8c5e3ab8ebbfcc477
Paul Wood <pmwood@pm.me>
2022-08-29 22:04:04 -0700

    A very creative message

--- lua/plmwd/keymaps.lua
+++ lua/plmwd/keymaps.lua
@@ -1,7 +1,8 @@
 local map = vim.keymap.set
 local utils = require 'plmwd.utils'
 local nmap, tmap, vmap = utils.nmap, utils.tmap, utils.vmap
-local home = vim.loop.os_homedir()
+local git = require 'plmwd.git'
+local packer = require 'packer'
 
 nmap(':', ';', { silent = false })
 nmap(';', ':', { silent = false })
@@ -31,20 +32,32 @@ nmap('<leader>9', '9gt')
 nmap('<leader>gg', '<cmd>Neogit<cr>')
 nmap('<leader>gd', '<cmd>DiffviewOpen<cr>')
 nmap('<leader>gD', function()
-    vim.ui.input({ prompt = 'git path: ' }, function(input)
-        vim.cmd('DiffviewOpen ' .. input)
-    end)
+  vim.ui.input({ prompt = 'git path: ' }, function(input)
+    vim.cmd('DiffviewOpen ' .. input)
+  end)
 end)
 
 nmap('<leader>ps', '<cmd>PackerSync<cr>')
 nmap('<leader>pc', '<cmd>PackerCompile<cr>')
+nmap('<leader>pp', function()
+  git.pull({ rebase = true })
+  packer.sync()
+end)
+
+nmap('<leader>pP', function()
+  git.context({ cwd = utils.config_dir }, function()
+    git.pull({ rebase = true })
+    git.commit({ all = true, mes = 'A very creative message' })
+    git.push()
+  end)
+end)
 
 nmap('<leader>cn', '<cmd>Telescope find_files cwd=~/.config/nvim<cr>')
 nmap('<leader>ck', '<cmd>Telescope find_files cwd=~/.config/kitty<cr>')
 
 nmap('<leader>[', '<cmd>NvimTreeFindFile<cr>')
 nmap('<leader>{', function()
-    require('nvim-tree.api').tree.toggle(true, true, vim.fn.getcwd())
+  require('nvim-tree.api').tree.toggle(true, true, vim.fn.getcwd())
 end)
 
 nmap('<leader>ff', '<cmd>Telescope find_files<cr>')
@@ -73,35 +86,35 @@ nmap('<leader>wL', '<c-w>L')
 
 -- Let <leader>tnn toggle the line column and remember if rnu was set
 nmap('<leader>tnn', function()
-    if vim.opt.nu:get() or vim.opt.rnu:get() then
-        vim.g.was_rnu = vim.opt.rnu:get()
-        vim.opt.rnu = false
-        vim.opt.nu = false
-    else
-        vim.opt.rnu = vim.g.was_rnu
-        vim.opt.nu = true
-    end
+  if vim.opt.nu:get() or vim.opt.rnu:get() then
+    vim.g.was_rnu = vim.opt.rnu:get()
+    vim.opt.rnu = false
+    vim.opt.nu = false
+  else
+    vim.opt.rnu = vim.g.was_rnu
+    vim.opt.nu = true
+  end
 end)
 
 -- Toggle rnu if nu is set, else enable rnu and nu since I like seeing the current line number
 nmap('<leader>tnr', function()
-    if vim.opt.nu:get() then
-        vim.opt.rnu = not vim.opt.rnu:get()
-        vim.g.was_rnu = not vim.g.was_rnu
-    else
-        vim.opt.rnu = true
-        vim.opt.nu = true
-        vim.g.was_rnu = true
-    end
+  if vim.opt.nu:get() then
+    vim.opt.rnu = not vim.opt.rnu:get()
+    vim.g.was_rnu = not vim.g.was_rnu
+  else
+    vim.opt.rnu = true
+    vim.opt.nu = true
+    vim.g.was_rnu = true
+  end
 end)
 
 nmap('<leader>tdv', function()
-    local lines_present, lsp_lines = pcall(require, 'lsp_lines')
-    if lines_present then
-        lsp_lines.toggle()
-    else
-        vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })
-    end
+  local lines_present, lsp_lines = pcall(require, 'lsp_lines')
+  if lines_present then
+    lsp_lines.toggle()
+  else
+    vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })
+  end
 end)
 
 nmap('<leader>xx', '<cmd>Trouble document_diagnostics<cr>')
@@ -113,8 +126,8 @@ nmap('<leader>tdh', vim.diagnostic.hide)
 nmap('<leader>R', utils.reload)
 
 nmap('<leader>e', function()
-    local file_dir = vim.fn.expand('%:p:h')
-    vim.cmd('Telescope file_browser path=' .. file_dir)
+  local file_dir = vim.fn.expand('%:p:h')
+  vim.cmd('Telescope file_browser path=' .. file_dir)
 end)
 
 tmap('<c-j><c-k>', '<c-\\><c-n>')
