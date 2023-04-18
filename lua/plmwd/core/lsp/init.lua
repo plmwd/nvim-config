e729f1b53f3760be7063ce21b0a0c2a2b491c287
Paul Wood <p.wood@samsung.com>
2023-04-18 10:08:49 -0700

    Fix error introduced

--- lua/plmwd/core/lsp/init.lua
+++ lua/plmwd/core/lsp/init.lua
@@ -169,18 +169,8 @@ return {
     {
         "williamboman/mason.nvim",
         cmd = "Mason",
+        main = 'mason',
         keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
         opts = {},
-        ---@param opts MasonSettings | {ensure_installed: string[]}
-        config = function(plugin, opts)
-            require("mason").setup(opts)
-            local mr = require("mason-registry")
-            for _, tool in ipairs(opts.ensure_installed) do
-                local p = mr.get_package(tool)
-                if not p:is_installed() then
-                    p:install()
-                end
-            end
-        end,
     },
 }
